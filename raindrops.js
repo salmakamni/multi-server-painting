af00b5af62570b46f1977fd119457f757c05ff4a
Glitch (hello-webpage) <none>
2020-05-20 17:19:40 +0000

    🔔🏃 Checkpoint ./raindrops.js:16411136/1936

--- raindrops.js
+++ raindrops.js
@@ -5,37 +5,44 @@
           collideCircleCircle, text, mouseX, mouseY, 
           strokeWeight, line, mouseIsPressed, noStroke */
 
+let drops =[]
 
-let drop1, drop2, drop3, drop4
 function setup() {
   createCanvas(500, 500)
   colorMode(HSB, 100);
-  // Variables for droplet 1
-  drop1x = 200 // or random(width)
-  drop1y = 0 // or random(height)
-  drop1d = 10 // or random(5,15)
-  drop1FallSpeed = 8 // or random(8, 20)
-
-  // Variables for droplet 2
+  for(let i =0; i < 30; i++) {
+  drops.push(new RainDrop());
+  }
+  
 }
 
 function draw() {
   background(0, 0, 95)
-  //// Code for droplet 1
-  // Move droplet 1
-  drop1y += drop1FallSpeed
-  // If it goes off the screen...
-  if (drop1y > height) {
-    // ...reset it...
-    drop1y = 0
-    // ...and move it somewhere random.
-    drop1x = random(width)
+  for(let i =0; i < drops.length; i++) {
+  drops[i].drip()
+    drops[i].show()
   }
-  // Display droplet 1
-  noStroke()
-  fill(60, 80, 80)
-  ellipse(drop1x, drop1y, drop1d)
+}
 
-  //// Code for droplet 2
-  // Code your next droplet here
+class RainDrop {
+  constructor() {
+    this.d = random(5,15)
+    this.x = random(width)
+    this.y = random(height)
+    this.fallSpeed = random(5,15)
+  }
+  
+  show() {
+    noStroke()
+    fill(60, 80, 80)
+    ellipse(this.x, this.y, this.d)
+  }
+  
+  drip() {
+    this.y += this.fallSpeed
+    if (this.y > height) {
+      this.y = 0
+      this.x = random(width)
+    }
+  }
 }
