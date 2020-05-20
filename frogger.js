e948884260a6437bc5b7f46abe5e089d12fb14e3
Glitch (hello-webpage) <none>
2020-05-20 16:33:46 +0000

    🏊🏼🌖 Checkpoint ./frogger.js:16411136/3685

--- frogger.js
+++ frogger.js
@@ -13,8 +13,8 @@ function setup() {
   createCanvas(500, 500)
   colorMode(HSB, 360, 100, 100)
   backgroundColor = 95
-  frogX = 250
-  frogY = 450
+  frogX = width / 2
+  frogY = height - 30
   score = 0
   lives = 3
   gameIsOver = false
@@ -25,8 +25,10 @@ function setup() {
 
 function draw() {
   background(backgroundColor)
+  // Code for gold goal line
   fill(60, 80, 80)
   rect(0, 0, width, 50)
+  // Code to display Frog
   fill(120, 80, 80)
   ellipse(frogX, frogY, 20)
   moveCars()
@@ -37,82 +39,47 @@ function draw() {
 }
 
 function keyPressed() {
-  if (gameIsOver) {
-    console.log("Game over - user input ignored.")
-  } else if (keyCode === UP_ARROW) {
-    frogY -= 10
-  } else if (keyCode === LEFT_ARROW) {
-    frogX -= 10
-  } else if (keyCode === RIGHT_ARROW) {
-    frogX += 10
+  if (keyCode === UP_ARROW) {
+    frogY -= 20
   } else if (keyCode === DOWN_ARROW) {
-    frogY += 10
+    frogY += 20
+  } else if (keyCode === LEFT_ARROW) {
+    frogX -= 20
+  } else if (keyCode === RIGHT_ARROW) {
+    frogX += 20
   }
 }
 
 function moveCars() {
-  car1X += car1V
-  if (car1X >= width) {
-    car1X = -30
-    // Move the car to a random vertical location
-    car1Y = random(100, 400)
-  }
+  // Move the car
+
+  // Reset if it moves off screen
+
 }
 
 function drawCars() {
+  // Code for car 1
   fill(0, 80, 80)
   rect(car1X, car1Y, 40, 30)
+  // Code for additional cars
 }
 
 function checkCollisions() {
-  // Check for Car 1
-  if (collideRectCircle(car1X, car1Y, 40, 30, frogX, frogY, 20)) {
-    console.log("collided with Car 1")
-    frogY = 450
-    lives -= 1
-  }
-  if (lives <= 0) {
-    gameIsOver = true
-  }
+  // If the frog collides with the car, reset the frog and subtract a life.
+
 }
 
 function checkWin() {
-  if (frogY <= 50) {
-    score += 1
-    frogY = 450
-    // Make the car faster each time you score (increasing difficulty per level)
-    car1V += 2
-  }
-  if (score === 5) {
-    gameIsOver = true
-  }
+  // If the frog makes it into the yellow gold zone, increment the score and move the frog back down to the bottom.
 }
 
 function displayScores() {
   textSize(12)
   fill(0)
   // Display Lives
-  text(`Lives: `, 10, 20)
-  fill(120, 80, 80)
-  if (lives >= 1) {
-    ellipse(50, 16, 10, 10)
-  }
-  if (lives >= 2) {
-    ellipse(65, 16, 10, 10)
-  }
-  if (lives >= 3) {
-    ellipse(80, 16, 10, 10)
-  }
+  text(`Lives: ${lives}`, 10, 20)
   // Display Score
-  fill(0)
-  text(`Score: ${score}`, 10, 38)
-  // Display game over message, win or lose
-  if (gameIsOver) {
-    textSize(60)
-    if (lives >= 1) {   
-      text("YOU WIN!", 110, height/2)
-    } else {
-      text("GAME OVER", 70, height/2)
-    }
-  }
+
+  // Display game over message if the game is over
+
 }
