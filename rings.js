145f95dad032265c92395e83634d34d597b8b12d
Glitch (hello-webpage) <none>
2020-07-06 01:45:05 +0000

    🚀⛄️ Checkpoint ./index.html:16411136/2160 ./rings.js:16411136/627

--- rings.js
+++ rings.js
@@ -5,35 +5,15 @@
           collideCircleCircle, text, mouseX, mouseY,
           noFill, strokeWeight */
 
-
-function setup() {
-  createCanvas(400, 400);
+function setup(){
+  // Code here runs only once
+  createCanvas(800, 600)
 }
 
-function draw() {
-  background(220);
+function draw(){
+  // Code here runs continuously
+  background(220)
 
-  // Brush settings
-  noFill()
-  strokeWeight(5)
+  // ellipse(50, 50, 50, 50)
 
-  // Ring 1: Blue
-  stroke(10, 134, 205)
-  ellipse(50, 50, 50)
-
-  // Ring 2: Yellow
-  stroke(255, 214, 0)
-  ellipse(80, 80, 50)
-
-  // Ring 3: Black
-  stroke(0, 0, 0)
-  ellipse(110, 50, 50)
-
-  // Ring 4: Green
-  stroke(33, 176, 76)
-  ellipse(140, 80, 50)
-
-  // Ring 5:
-  stroke(234, 30, 35)
-  ellipse(170, 50, 50)
 }
